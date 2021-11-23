b112ee916806bed1cd865a41fc46e9dff674a2eb
Victor <victor.dualibi@zup.com.br>
2021-11-23 17:08:19 -0300

    refactor: fixed test_features.py arguments

--- tests/test_features.py
+++ tests/test_features.py
@@ -17,7 +17,7 @@ def test_type_target_diff_transformer(sample_input_data):
 def test_shape_target_diff_transformer(sample_input_data):
     transformer = features.TargetDiffTransformer(*config.model_config.diff_create_var)
 
-    expected_df_shape = (49, 7)
+    expected_df_shape = (40, 10)
     subject = transformer.fit_transform(sample_input_data)
 
     assert subject.shape == expected_df_shape
@@ -26,7 +26,7 @@ def test_shape_target_diff_transformer(sample_input_data):
 def test_value_target_diff_transformer(sample_input_data):
     transformer = features.TargetDiffTransformer(*config.model_config.diff_create_var)
 
-    expected_value = -42.0
+    expected_value = -85.0
     subject = transformer.fit_transform(sample_input_data)
 
     assert subject[config.model_config.target_dg][0] == expected_value
